04ad0a6f1db00af6d61ee9b57c6ae2fac97be756
DESKTOP-753E30S\你好 <123147769@qq.com>
2017-12-21 11:22:01 +0800

    map

---
+++ back.js
@@ -0,0 +1,23 @@
+// var polygon = new BMap.Polygon([
+//		new BMap.Point(151.861582,50.112295),
+//		new BMap.Point(164.224565,43.021168),
+//		new BMap.Point(169.22863,33.77793),
+//		new BMap.Point(170.406057,23.679565),
+//		new BMap.Point(168.34556,9.530272),
+//		new BMap.Point(160.692284,4.236674),
+//		new BMap.Point(151.861582,1.276858),
+//		new BMap.Point(138.615528,1.276858),
+//		new BMap.Point(125.369474,2.16553),
+//		new BMap.Point(108.296783,6.007581),
+//		new BMap.Point(100.349151,18.712367),
+//		new BMap.Point(97.69994,34.512177),
+//		new BMap.Point(100.643507,44.303315),
+//		new BMap.Point(112.417777,49.732198),
+//		new BMap.Point(123.308977,50.863468),
+//		new BMap.Point(134.200177,51.23456),
+//		new BMap.Point(142.147809,51.23456),
+//	], {fillColor:"#ff0000",strokeColor:"red", strokeWeight:1, strokeOpacity:1});  //创建多边形
+//	map.addOverlay(polygon);   //增加多边形
+//	polygon.addEventListener("click",function(){
+//		 
+//	});
